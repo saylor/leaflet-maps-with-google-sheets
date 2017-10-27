603c68cb343c936e4483476f01f6e83eee4638ca
Steve Aylor <saylor@opubco.com>
2017-10-27 15:46:01 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1okUkT23Q8XO1Rc-NVHX3mvKDkKJRcnwlW1d-kcilewU/edit#gid=0';
